ecc08210b7d710224301e9471c277a9cb4818f08
AW Admin <aw-admin@karinvalenzuela1966outlook.onmicrosoft.com>
2024-11-20 23:58:35 +0000

    Committing 4 items from workspace 9e7bf7af-e042-4baa-8e61-8dbb2a717c9f

---
+++ Sales_Silver_To_Gold.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "f267edbc-42c2-4c08-87f5-f8f6380c9f4c",
+# META       "default_lakehouse_name": "Sales_Bronze",
+# META       "default_lakehouse_workspace_id": "9e7bf7af-e042-4baa-8e61-8dbb2a717c9f",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "f267edbc-42c2-4c08-87f5-f8f6380c9f4c"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
